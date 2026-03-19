5a05399738d72c3ed044c7ff0c2b964efd0a2141
raitulsams <raitulsams202@gmail.com>
2026-03-19 05:51:55 +0100

    connection to mongodb securely done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bberrop.mongodb.net/?appName=Cluster0`;
-require('dotenv').config()
+
 
 // middleware
 app.use(cors());
